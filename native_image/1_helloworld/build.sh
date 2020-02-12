5d6d425229437243159690361ba9d4e7510ff563
u230412 <matthias.baumeler@sbb.ch>
2020-02-12 21:58:44 +0100

    Build into target folder

--- native_image/1_helloworld/build.sh
+++ native_image/1_helloworld/build.sh
@@ -1,2 +1,7 @@
-javac HelloWorld.java
-jar cfe runnable.jar HelloWorld *.class
+#!/bin/bash
+
+mkdir -p target
+javac -d target src/*.java
+jar cfe target/runnable.jar HelloWorld  -C target .
+$GRAALVM_HOME/bin/native-image -jar target/runnable.jar target/executable
+
