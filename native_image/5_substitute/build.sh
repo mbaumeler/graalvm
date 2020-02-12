91cd4439948392c1b8137df40bff374f86b754d3
u230412 <matthias.baumeler@sbb.ch>
2020-02-12 22:21:23 +0100

    Build into target folder

--- native_image/5_substitute/build.sh
+++ native_image/5_substitute/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
-$GRAALVM_HOME/bin/javac -classpath .:$GRAALVM_HOME/jre/lib/svm/builder/svm.jar *.java 
-jar cfe runnable.jar Main *.class
-$GRAALVM_HOME/bin/native-image -jar runnable.jar --no-fallback executable 
+
+mkdir -p target
+$GRAALVM_HOME/bin/javac -d target -classpath .:$GRAALVM_HOME/jre/lib/svm/builder/svm.jar src/*.java 
+jar cfe target/runnable.jar Main -C target .
+$GRAALVM_HOME/bin/native-image -jar target/runnable.jar --no-fallback target/executable 
