cf47116581c74e5bd8b729d96c3a42d6be5af1c1
u230412 <matthias.baumeler@sbb.ch>
2020-02-12 22:40:55 +0100

    Build into target folder

--- native_image/6_initialize_at_build_time/build.sh
+++ native_image/6_initialize_at_build_time/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e
-javac *.java
-jar cfe runnable.jar Main *.class countries.txt
-$GRAALVM_HOME/bin/native-image --initialize-at-build-time -jar runnable.jar --no-fallback executable 
+
+mkdir -p target
+javac -d target src/*.java
+jar cfe target/runnable.jar Main -C src countries.txt -C target .
+$GRAALVM_HOME/bin/native-image --initialize-at-build-time -jar target/runnable.jar --no-fallback target/executable 
